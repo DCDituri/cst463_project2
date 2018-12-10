28764a116033c9f0f0da7cf96ee6049ae5f666b5
Dallas Dituri <dcdituri@hotmail.com>
2018-12-10 14:23:33 -0800

    here ya go

--- part1.py
+++ part1.py
@@ -17,18 +17,35 @@ ix_to_char = { i:ch for i,ch in enumerate(chars) }
 hidden_size = 100 # size of hidden layer of neurons
 seq_length = 25 # number of steps to unroll the RNN for
 learning_rate = 1e-1
+
+# preprocessing
+X = []
+y = []
+for i in range (0, len(data)-seq_length-1, 1):
+    X.append([char_to_ix[ch] for ch in data[i:i+seq_length]])
+    y.append([char_to_ix[ch] for ch in data[i+1:i+seq_length+1]])
+X_modified = np.reshape(X, (len(X), seq_length))
+y_modified = np.reshape(y, (len(y), seq_length))
+
 X0_weight = np.random.randn(hidden_size, vocab_size)*0.01
 X1_weight = np.random.randn(hidden_size, hidden_size)*0.01
 X2_weight = np.random.randn(vocab_size, hidden_size)*0.01
+
+tf.reset_default_graph()
 # model parameters
 X0 = tf.placeholder(tf.float32, [None, seq_length]) # input to hidden
 # X1 = tf.placeholder(tf.float32, [None, seq_length]) # hidden to hidden
 y = tf.placeholder(tf.float32, [None, seq_length])
-cell = tf.contrib.rnn.BasicRNNCell(num_units=hidden_size, activation=tff.nn.leaky_relu)
+rnn_cell = tf.nn.rnn_cell.BasicRNNCell(num_units=hidden_size)
+outputs, states = tf.nn.dynamic_rnn(rnn_cell, X0, dtype=tf.float32)
 
 # Wx0 = tf.Variable(tf.random_normal(shape=[seq_length, hidden_size],dtype=tf.float32))
 # Wx1 = tf.Variable(tf.random_normal(shape=[hidden_size, hidden_size],dtype=tf.float32))
 # Wy = tf.Variable(tf.random_normal(shape=[hidden_size, seq_length],dtype=tf.float32))
 # hidden_bias = tf.Variable(tf.zeros([1, hidden_size], dtype=tf.float32))
 # output_bias = tf.Variable(tf.zeros([1, vocab_size], dtype=tf.float32))
-
+init = tf.gloabal_varaibles_initializer()
+with tf.Session() as sess:
+    init.run()
+    outputs_val = outputs.eval(feed_dict={X: X_modified})
+    
